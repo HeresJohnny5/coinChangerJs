12a8eae3db8e4f6428ce129d4b8437a52e233841
John Erickson <ericksonjohn08@hotmail.com>
2017-05-12 22:14:59 -0400

    Add input field and onclick functionality to home.html. Add currencyFunction to scripts/app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,44 @@
-var userInput = prompt('Enter a coin amount.');
-userInput = parseInt(userInput);
+function currencyFunction() {
+	var userInput = document.getElementById('number').value;
+	userInput = userInput * 100;
+	userInput = userInput.toFixed(0);
+	userInput = parseInt(userInput);
+	
+	var currency = {
+		tenDollars: 0,
+		fiveDollars: 0,
+		dollar: 0,
+		quarter: 0,
+		dime: 0,
+		nickel: 0,
+		penny: 0
+	};
 
-console.log(userInput);
-
-var currency = {
-	tenDollars: 0,
-	fiveDollars: 0,
-	dollar: 0,
-	quarter: 0,
-	dime: 0,
-	nickel: 0,
-	penny: 0
-};
-
-while(userInput > 0) {	
-	if(userInput >= 1000) {
-		currency.tenDollars++;
-		userInput = userInput - 1000;
-	} else if(userInput >= 500) {
-		currency.fiveDollars++;
-		userInput = userInput - 500;
-	} else if(userInput >= 100) {
-		currency.dollar++;
-		userInput = userInput - 100;
-	} else if(userInput >= 25) {
-		currency.quarter++;
-		userInput = userInput - 25;
-	} else if(userInput >= 10) {
-		currency.dime++;
-		userInput = userInput - 10;
-	} else if(userInput >= 5) {
-		currency.nickel++;
-		userInput = userInput - 5;
-	} else if(userInput > 0) {
-		currency.penny++;
-		userInput = userInput - 1;
+	while(userInput > 0) {	
+		if(userInput >= 1000) {
+			currency.tenDollars++;
+			userInput = userInput - 1000;
+		} else if(userInput >= 500) {
+			currency.fiveDollars++;
+			userInput = userInput - 500;
+		} else if(userInput >= 100) {
+			currency.dollar++;
+			userInput = userInput - 100;
+		} else if(userInput >= 25) {
+			currency.quarter++;
+			userInput = userInput - 25;
+		} else if(userInput >= 10) {
+			currency.dime++;
+			userInput = userInput - 10;
+		} else if(userInput >= 5) {
+			currency.nickel++;
+			userInput = userInput - 5;
+		} else if(userInput > 0) {
+			currency.penny++;
+			userInput = userInput - 1;
+		}
 	}
+	
+	console.log(currency);
 }
 
-console.log(currency);
